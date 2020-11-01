46b009694c1a92c6d991dc7469d54da8893d5495
lsw725 <725lsw@hanmail.net>
2020-11-02 00:59:31 +0900

    add bottomPanel

--- javadb/src/javadb/DBFrame.java
+++ javadb/src/javadb/DBFrame.java
@@ -9,15 +9,19 @@ import javax.swing.JTabbedPane;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
+import java.io.*;
+
 public class DBFrame extends JFrame{
 	public DBFrame(){
 		// Frame을 생성
 		this.setTitle("Java DB Program");
 		
 		Container contentPane = getContentPane();
-		JPanel optionPanel = new OptionPanel();
-		JPanel resultPanel = new ResultPanel();
+		OptionPanel optionPanel = new OptionPanel();
+		ResultPanel resultPanel = new ResultPanel();
+		BottomPanel bottomPanel = new BottomPanel();
 		contentPane.add(optionPanel, BorderLayout.NORTH);
 		contentPane.add(resultPanel, BorderLayout.CENTER);
+		contentPane.add(bottomPanel, BorderLayout.SOUTH);
 	}
 }
